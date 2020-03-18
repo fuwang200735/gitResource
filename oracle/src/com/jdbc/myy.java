6ed12308ca7c9cbeecdb7f18bff2b1f04df4433e
myy <1715865383@qq.com>
2020-03-18 17:06:05 +0800

    myy myy001

--- oracle/src/com/jdbc/myy.java
+++ oracle/src/com/jdbc/myy.java
@@ -8,8 +8,8 @@ public static void main(String[] args) throws SQLException {
 	 String sql="select * from emp";
 	 ResultSet resultSet=JDBCUtils.executeQuery(sql, null);
 	 if(resultSet.next()) {
-		 System.out.println(resultSet.getInt("empno"));
-		 System.out.println(resultSet.getString("ename"));
+		 System.out.println(resultSet.getInt("MYY"));
+		 System.out.println(resultSet.getString("MYY"));
 	 }
 	 
 }
